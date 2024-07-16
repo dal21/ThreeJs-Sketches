5b39f9b91c3105013ec75d0a52064661bf4a3798
dal21 <david.callaghan@hotmail.co.uk>
2024-07-16 22:25:10 +0100

    first commit

---
+++ getting-started-with-threejs-main/index.js
@@ -0,0 +1,262 @@
+import * as THREE from "three";
+import { FBXLoader } from '../node_modules/three/examples/jsm/loaders/FBXLoader.js';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from '../node_modules/three/examples/jsm/loaders/RGBELoader.js';
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const w = window.innerWidth;
+const h = window.innerHeight;
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 20;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+const scene = new THREE.Scene();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableRotate = true;
+controls.enableZoom = true;
+controls.enablePan = false;
+controls.enableDamping = true;
+controls.dampingFactor = 0.2;
+controls.screenSpacePanning = false;
+controls.minDistance = 0.1;
+controls.maxDistance = 10;
+controls.maxPolarAngle = Math.PI / 2;
+
+// Load the HDRI environment map
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load('../stars_4k.hdr', (texture) => {
+    texture.mapping = THREE.EquirectangularRefractionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+}, undefined, (error) => {
+    console.error('Error loading HDRI:', error);
+});
+
+// Directional light (simulating sunlight)
+const dirLight = new THREE.DirectionalLight(0xffFA7F08, 0.8);
+dirLight.position.set(-5, 5, -5);
+dirLight.castShadow = true;
+scene.add(dirLight);
+
+// Shadow configuration for the directional light
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+dirLight.shadow.camera.top = 10;
+dirLight.shadow.camera.bottom = -10;
+dirLight.shadow.camera.left = -10;
+dirLight.shadow.camera.right = 10;
+dirLight.shadow.camera.near = 0.01;
+dirLight.shadow.camera.far = 20;
+dirLight.shadow.bias = -0.001;
+
+// New Directional Light
+const dirLight2 = new THREE.DirectionalLight(0xFFE4B5, 0.3);
+dirLight2.position.set(5, 0, 5);
+dirLight2.castShadow = true;
+scene.add(dirLight2);
+
+// Shadow configuration for the second directional light
+dirLight2.shadow.mapSize.width = 2048;
+dirLight2.shadow.mapSize.height = 2048;
+dirLight2.shadow.camera.top = 10;
+dirLight2.shadow.camera.bottom = -10;
+dirLight2.shadow.camera.left = -10;
+dirLight2.shadow.camera.right = 10;
+dirLight2.shadow.camera.near = 0.01;
+dirLight2.shadow.camera.far = 20;
+dirLight2.shadow.bias = -0.001;
+
+// Add a SpotLight
+const intensity = 5;
+const decay = 2.5;
+const distance = 10;
+const angle = 0.9;
+const penumbra = 1;
+const spotLight = new THREE.SpotLight(0xff22BABB, intensity, distance, angle, penumbra);
+spotLight.position.set(-0.1, -0.15, 0.15);
+spotLight.castShadow = true;
+
+const target = new THREE.Object3D();
+target.position.set(0.1, -2, 2);
+scene.add(target);
+
+spotLight.target = target;
+
+// Optional: Set shadow properties
+spotLight.shadow.mapSize.width = 2048;
+spotLight.shadow.mapSize.height = 2048;
+spotLight.shadow.camera.near = 0.1;
+spotLight.shadow.camera.far = 50;
+
+scene.add(spotLight);
+
+// Audio listener
+const listener = new THREE.AudioListener();
+camera.add(listener);
+
+// Create a video element
+const video = document.createElement('video');
+video.src = '../Field Music - Orion From The Street (Official Video).mp4';
+video.loop = true;
+video.muted = false;
+video.play();
+
+// Create a video texture
+const videoTexture = new THREE.VideoTexture(video);
+
+// Create a plane geometry for the video
+const videoGeometry = new THREE.PlaneGeometry(0.35, 0.25);
+const videoMaterial = new THREE.MeshStandardMaterial({
+    map: videoTexture,
+    emissive: 0xffffff,
+    emissiveMap: videoTexture,
+    emissiveIntensity: 1.5
+});
+const videoPlane = new THREE.Mesh(videoGeometry, videoMaterial);
+videoPlane.position.set(-0.165, -0.130, 0.0445);
+videoPlane.rotation.set(0, 0.2000, 0);
+scene.add(videoPlane);
+
+// Load the GLTF model
+const loader = new GLTFLoader();
+loader.load('../FMAR_TV_TEST_10.gltf', function (gltf) {
+    gltf.scene.scale.multiplyScalar(10 / 1);
+    gltf.scene.position.x = 0;
+    gltf.scene.position.z = 0;
+    scene.add(gltf.scene);
+});
+
+renderer.shadowMap.enabled = true;
+scene.traverse((node) => {
+    if (node.isMesh) {
+        node.receiveShadow = true;
+    }
+});
+
+// Texture animation setup for the first plane
+const manager1 = new THREE.LoadingManager();
+const textureLoader1 = new THREE.TextureLoader(manager1);
+const textures1 = [];
+const numImages1 = 88;
+
+for (let i = 0; i < numImages1; i++) {
+    textures1.push(textureLoader1.load(`../ImageSequence/Star1/Starshape/Starshape_${i}.png`));
+}
+
+const animationGeometry1 = new THREE.PlaneGeometry(1, 1);
+const animationMaterial1 = new THREE.MeshStandardMaterial({
+    map: textures1[0],
+    transparent: true,
+    emissive: 0xffffff,  // Set the emissive color to white
+    emissiveMap: textures1[0],  // Use the same texture for emissive map
+    emissiveIntensity: 1.5  // Adjust the intensity of the emissive effect
+});
+const animatedMesh1 = new THREE.Mesh(animationGeometry1, animationMaterial1);
+animatedMesh1.position.set(-0.6, 0.5, 0.25);
+animatedMesh1.rotation.set(0, 1.5, 0);
+animatedMesh1.scale.set(0.5, 0.5, 0.5);
+scene.add(animatedMesh1);
+
+// Texture animation setup for the second plane
+const manager2 = new THREE.LoadingManager();
+const textureLoader2 = new THREE.TextureLoader(manager2);
+const textures2 = [];
+const numImages2 = 146;
+
+for (let i = 0; i < numImages2; i++) {
+    textures2.push(textureLoader2.load(`../ImageSequence/Star1/PaperFolding/PaperSpin_${i}.png`));
+}
+
+const animationGeometry2 = new THREE.PlaneGeometry(1, 1);
+const animationMaterial2 = new THREE.MeshStandardMaterial({
+    map: textures2[0],
+    transparent: true,
+    emissive: 0xffffff,  // Set the emissive color to white
+    emissiveMap: textures2[0],  // Use the same texture for emissive map
+    emissiveIntensity: 1  // Adjust the intensity of the emissive effect
+});
+const animatedMesh2 = new THREE.Mesh(animationGeometry2, animationMaterial2);
+animatedMesh2.position.set(-0.2, 0.26, -0.15);
+animatedMesh2.rotation.set(0, 0.25, 0);
+animatedMesh2.scale.set(0.5, 0.5, 0.5);
+scene.add(animatedMesh2);
+
+// Animation management for both planes
+const frameRate = 12;
+const frameDuration = 1000 / frameRate;
+const delayDuration = 0;  // Number of frames to delay the restart of the animation
+const delayDuration2 = 48;  // Number of frames to delay the restart of the animation
+let lastFrameTime = Date.now();
+let currentFrame1 = 0;
+let currentFrame2 = 0;
+let delayCounter1 = delayDuration;
+let delayCounter2 = delayDuration2;
+
+manager1.onLoad = manager2.onLoad = function() {
+    console.log('All textures loaded');
+
+    function animate() {
+        requestAnimationFrame(animate);
+
+        // Update controls
+        controls.update();
+
+        // Add noise distortion to the spotlight's intensity
+        spotLight.intensity = intensity + (Math.random() - 0.5) * 0.5;
+
+        // Slow down position jitter
+        const jitterAmount = 0.0005;
+        spotLight.position.x += (Math.random() - 0.3) * jitterAmount;
+        spotLight.position.y += (Math.random() - 0.3) * jitterAmount;
+        spotLight.position.z += (Math.random() - 0.3) * jitterAmount;
+
+        const now = Date.now();
+        const elapsedTime = now - lastFrameTime;
+
+        if (elapsedTime >= frameDuration) {
+            lastFrameTime = now;
+
+            // Update the first animation
+            if (currentFrame1 < numImages1) {
+                animationMaterial1.map = textures1[currentFrame1];
+                animationMaterial1.emissiveMap = textures1[currentFrame1]; // Update emissiveMap as well
+                animationMaterial1.needsUpdate = true;
+                currentFrame1++;
+            } else if (delayCounter1 > 0) {
+                delayCounter1--;
+            } else {
+                currentFrame1 = 0;
+                delayCounter1 = delayDuration;
+            }
+
+            if (currentFrame2 < numImages2) {
+                animationMaterial2.map = textures2[currentFrame2];
+                animationMaterial2.emissiveMap = textures2[currentFrame2]; // Update emissiveMap as well
+                animationMaterial2.needsUpdate = true;
+                currentFrame2++;
+            
+            } else if (delayCounter2 > 0) {
+                delayCounter2--;
+            } else {
+                currentFrame2 = 0;
+                delayCounter2 = delayDuration;
+            }
+        }
+
+        // Render scene
+        renderer.render(scene, camera);
+    }
+
+    animate();
+};
+
+// Log the scene and camera to verify they are set up correctly
+console.log('Scene:', scene);
+console.log('Camera:', camera);
