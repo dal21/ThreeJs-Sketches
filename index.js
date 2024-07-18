408105e1d5860d40b2cb77134b1167670464fb7b
dal21 <38189650+dal21@users.noreply.github.com>
2024-07-18 15:27:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ function createInstances() {
 const loader7 = new GLTFLoader();
 let mixer3; // Second mixer
 loader7.load('./Wind Turbine.gltf', function (gltf) {
-    gltf.scene.scale.multiplyScalar(1);
+    gltf.scene.scale.multiplyScalar(0.3/1);
     gltf.scene.position.set(0, 0.1, -0.05);  // Set initial position
     scene.add(gltf.scene);
 
@@ -474,10 +474,12 @@ manager2.onLoad = function() {
         if (mixer2) {
             mixer2.update(delta);
         }
-        // Update each mixer
-          mixers.forEach((mixer) => {
-        mixer.update(delta);
-        });
+           // Update the second mixer if it exists
+           if (mixer3) {
+            mixer3.update(delta);
+        }
+  
+  
 
         // Update controls
         controls.update();
