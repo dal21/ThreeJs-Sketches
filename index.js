ba723d3db442e870fb82aff66f8f10d8e7d7eb5f
dal21 <38189650+dal21@users.noreply.github.com>
2024-07-18 18:57:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ const loader7 = new GLTFLoader();
 let mixer3; // Second mixer
 loader7.load('./Wind Turbine.gltf', function (gltf) {
     gltf.scene.scale.multiplyScalar(0.4 / 1);
-    gltf.scene.position.set(0, 0.05, -0.05);  // Set initial position
+    gltf.scene.position.set(0, 0.02, -0.05);  // Set initial position
     scene.add(gltf.scene);
 
      // Animation mixer to handle animations
